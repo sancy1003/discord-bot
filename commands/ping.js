a4afac9577d48b92af1970caf5c0558393d94382
chanyoung <kipa1003@naver.com>
2022-05-23 03:50:19 +0900

    봇 기본 세팅

---
+++ commands/ping.js
@@ -0,0 +1,6 @@
+//run이라는 메소드(function)을 export(수출)
+exports.run = (client, msg, args) => {
+  msg.reply(`${client.ws.ping}ms`);
+};
+
+exports.name = "ping";
---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const { Client, Intents, Collection } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const fs = require("fs");
+const prefix = "#";
+
+client.commands = new Collection();
+
+client.commands.load = (dir) => {
+  for (const file of fs.readdirSync(dir)) {
+    const cmd = require(`./commands/${file}`);
+    client.commands.set(cmd.name, cmd);
+  }
+};
+
+client.commands.load(__dirname + "/commands");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  if (msg.content.slice(0, prefix.length) !== prefix) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  let cmd = client.commands.get(command);
+  if (cmd) cmd.run(client, msg, args);
+});
+
+client.login(process.env.DISCORD_TOKKEN);
